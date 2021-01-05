d1e288c616ec7958af103cfb8bcabf4c7a284a4e
Philipp Spinnler <philipp@spinnler.ch>
2021-01-05 09:54:11 +0100

    persist refresh token

--- routes/withings.js
+++ routes/withings.js
@@ -2,14 +2,26 @@ const express = require('express');
 const router = express.Router();
 const axios = require("axios");
 const FormData = require('form-data');
+const fs = require('fs');
 
 let expiresAt = {};
 let accessToken = {}
 
+let refreshTokens = getRefreshTokens();
+
+function getRefreshTokens() {
+    return JSON.parse(fs.readFileSync('.withings_refresh_tokens'));
+}
+
+function setRefreshToken(person, token) {
+    const refreshToken = refreshTokens.find(refreshTokenTmp => refreshTokenTmp.person == person);
+    refreshToken.token = token === undefined ? null : token;
+    fs.writeFileSync(".withings_refresh_tokens", JSON.stringify(refreshTokens));
+}
+
 router.get('/', async (req, res) => {
     const finalResponse = {};
 
-    const refreshTokens = JSON.parse(process.env.MODULE_WITHINGS_REFRESH_TOKENS);
     for (const refreshToken of refreshTokens) {
         const getToken = async () => {
             if (expiresAt[refreshToken.person] && Date.now() < expiresAt[refreshToken.person]) {
@@ -21,13 +33,16 @@ router.get('/', async (req, res) => {
             formData.append('action', 'requesttoken');
             formData.append('client_id', process.env.MODULE_WITHINGS_CLIENT_ID);
             formData.append('client_secret', process.env.MODULE_WITHINGS_CLIENT_SECRET);
-            formData.append('grant_type', 'refresh_token')
-            formData.append('refresh_token', refreshToken.token);
+            formData.append('grant_type', 'refresh_token');
+            formData.append('refresh_token', refreshToken.token ? refreshToken.token : '');
 
             const responseToken = await axios.post('https://wbsapi.withings.net/v2/oauth2', formData, {
                 headers: formData.getHeaders()
             });
             expiresAt[refreshToken.person] = ((Date.now() / 1000) + responseToken.data.body.expires_in) * 1000;
+            
+            setRefreshToken(refreshToken.person, responseToken.data.body.refresh_token);
+
             return responseToken.data.body.access_token;
         }
 
