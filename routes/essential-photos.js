7e1ed14815abec760cac91ef0d7634391750f0ce
Philipp Spinnler <philipp@spinnler.ch>
2021-01-09 13:42:29 +0100

    New insta grapper

--- routes/essential-photos.js
+++ routes/essential-photos.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios').default;
+const Instagram = require('instagram-web-api')
 
 router.get('/', async (req, res) => {
     try {
-        const responseIG = await axios.get('https://www.instagram.com/essentialphotoscom/?__a=1');
-        const IGCount = responseIG.data.graphql.user.edge_followed_by.count;
+        const client = new Instagram({ username: process.env.MODULE_ESSENTIAL_PHOTOS_USERNAME, password: process.env.MODULE_ESSENTIAL_PHOTOS_PASSWORD })
+        await client.login()
+        const followers = await client.getFollowers({ userId: process.env.MODULE_ESSENTIAL_PHOTOS_FOLLOWERS_USER_ID })
 
         const responseFB = await axios.get('http://www.facebook.com/plugins/fan.php?connections=100&id=essentialphotoscom');
         const result = responseFB.data.match(/([0-9]*) „Gefällt mir“/);
         const FBCount = parseInt(result[1]);
         res.send({
             facebook: FBCount,
-            instagram: IGCount,
-            combinedFormatted: `FB: ${FBCount} | IG: ${IGCount}`
+            instagram: followers.count
         })
     } catch(e) {
-        res.status(204).send();
+        console.log(e);
     }
     
 });
