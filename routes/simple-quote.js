9619ab1ffd66efb001c1ae46a4fa9abf22bdbba6
Philipp Spinnler <philipp@spinnler.ch>
2021-12-17 21:00:54 +0100

    Only get new quote daily

--- routes/simple-quote.js
+++ routes/simple-quote.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios').default;
+const dayjs = require('dayjs');
+
+let lastUpdated = null;
+let cachedResponse = null;
 
 router.get('/', async (req, res) => {
 
-    const options = {
-        method: 'GET',
-        url: 'https://simplequote.spinnler.ch/api/simplequote.php',
-        auth: {
-            username: process.env.MODULE_SIMPLE_QUOTE_USERNAME,
-            password: process.env.MODULE_SIMPLE_QUOTE_PASSWORD
-        }
-    };
-
-    const response = await axios.request(options);
-
-    const parsed = response.data;
+    if (!lastUpdated || !dayjs().isSame(lastUpdated, 'day')) {
+        const options = {
+            method: 'GET',
+            url: 'https://simplequote.spinnler.ch/api/simplequote.php',
+            auth: {
+                username: process.env.MODULE_SIMPLE_QUOTE_USERNAME,
+                password: process.env.MODULE_SIMPLE_QUOTE_PASSWORD
+            }
+        };
     
-    res.send(parsed);
+        const response = await axios.request(options);
+    
+        cachedResponse = response.data;
+
+        lastUpdated = dayjs()
+    }
+     
+    res.send(cachedResponse);
 });
 
 module.exports = router;
