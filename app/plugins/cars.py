2134687e3ad0f6adfccbe195c0b0c890f3da1f80
Philipp Spinnler <philipp@spinnler.ch>
2025-10-27 11:17:29 +0100

    Changes in cars plugin

--- app/plugins/cars.py
+++ app/plugins/cars.py
@@ -17,20 +17,18 @@ async def get_data():
             for car_config in cars_config:
                 car_name = car_config.get('name', 'Unknown Car')
                 range_entity = car_config.get('range_entity')
-                charging_active = car_config.get('charging_active_entity')
-                charging_power = car_config.get('charging_power_entity')
+                charging_active_entity = car_config.get('charging_active_entity')
+                charging_power_entity = car_config.get('charging_power_entity')
                 end_of_charge_entity = car_config.get('end_of_charge_entity')
                 state_of_charge_entity = car_config.get('state_of_charge_entity')
-
-                range = client.get_entity(entity_id=range_entity)
 
                 # Initialize car entry
                 car_entry = {
                     "name": car_name,
                     "range": client.get_entity(entity_id=range_entity).get_state().state,
                     "charge_procentage": client.get_entity(entity_id=state_of_charge_entity).get_state().state,
-                    "charging": True if client.get_entity(entity_id=charging_active).get_state().state == "on" else False,
-                    "charging_power": client.get_entity(entity_id=charging_power).get_state().state,
+                    "charging": True if client.get_entity(entity_id=charging_active_entity).get_state().state == "on" else False,
+                    "charging_power": client.get_entity(entity_id=charging_power_entity).get_state().state,
                     "end_of_charge": client.get_entity(entity_id=end_of_charge_entity).get_state().state,
                 }
                 
