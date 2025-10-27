0dfc882d1c1a5ca913c1f015023b33cfc8899c59
Philipp Spinnler <philipp@spinnler.ch>
2025-10-27 10:12:35 +0100

    Changes in cars plugin

--- app/plugins/cars.py
+++ app/plugins/cars.py
@@ -17,7 +17,7 @@ async def get_data():
             for car_config in cars_config:
                 car_name = car_config.get('name', 'Unknown Car')
                 range_entity = car_config.get('range_entity')
-                charging_status_entity = car_config.get('charging_status_entity')
+                charging_active = car_config.get('charging_active_entity')
                 end_of_charge_entity = car_config.get('end_of_charge_entity')
                 state_of_charge_entity = car_config.get('state_of_charge_entity')
 
@@ -28,7 +28,7 @@ async def get_data():
                     "name": car_name,
                     "range": client.get_entity(entity_id=range_entity).get_state().state,
                     "charge_procentage": client.get_entity(entity_id=state_of_charge_entity).get_state().state,
-                    "charging_status": client.get_entity(entity_id=charging_status_entity).get_state().state, # 0 seems to be "charging"
+                    "charging": True if client.get_entity(entity_id=charging_active).get_state().state == "on" else False, # 0 seems to be "charging"
                     "end_of_charge": client.get_entity(entity_id=end_of_charge_entity).get_state().state,
                 }
                 
